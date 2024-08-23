19bd3133812c02899d8c28eb5120b7694fc4898c
PlmFerraz <140653429+PlmFerraz@users.noreply.github.com>
2024-08-23 14:26:10 -0300

    Atualização

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-/* PARÂMETROS
-Os parâmetros são os nomes que damos a valores que uma função pode receber em sua chamada, 
-que podem ou não ter um valor padrão. 
-Os parâmetros de uma função são como variáveis ficam disponíveis apenas no corpo da função.
-Os parâmetros servem para auxiliar em alguma lógica no código dentro de uma função. 
-Por exemplo, uma função calculaMedia(), pode ter como parâmetros notaA e notaB, que serão os valores utilizados para serem calculados e retornar o valor da média.
-*/
-
 function tocaSom(idElementoAudio) {
   document.querySelector(idElementoAudio).play();
 }
@@ -15,13 +7,12 @@ const listaDeTeclas = document.querySelectorAll(".tecla");
 let contador = 0;
 
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
-  const tecla = listaDeTeclas[contador]; //tecla atual
-  const instrumento = tecla.classList[1]; // classe da posição 1 (nome da tecla)
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
 
-  const idAudio = `#som_${instrumento}`; //concatenação (template string) do id com a classe
+  const idAudio = `#som_${instrumento}`;
 
   tecla.onclick = function () {
     tocaSom(idAudio);
   };
-   
 }
